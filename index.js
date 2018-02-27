e9a0068572ac69270f0436541216f1c6f219f318
yossi katzman <issoyk@gmail.com>
2018-02-27 18:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,6 @@ function iLoveTheBeatles(n) {
   do {
     array.push("I love the Beatles!")
   }
-  while (n < 15) {
-  array.push("I love the Beatles!")
-  n++
-  }
+  while (n < 15) 
   return array
 }
