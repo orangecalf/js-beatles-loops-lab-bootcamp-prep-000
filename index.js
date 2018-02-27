85f0857ed78dac8e2b59c6108b2c2cb6574ab9bb
yossi katzman <issoyk@gmail.com>
2018-02-27 17:35:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments) {
+ var ding = []
+  for (var lick = 0; lick < 4; lick++) {
+    if (lick < 3) {
+      ding.push(musicians[lick] + " plays " + instruments[lick])
+    }
+    else {
+       ding.push(musicians[lick] + " plays " + instruments[lick])
+      return ding
+    }
+  }
+}
+
+function johnLennonFacts(facts) {
+  var a = 0
+  var ping = []
+  var d = "!!!"
+  while (a > 3) {
+    ping.push(facts[a] + d)
+    a++
+  }
+  return ping
+}
