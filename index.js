f35077c959dcb83de344750e1b64e41eb5899655
yossi katzman <issoyk@gmail.com>
2018-02-27 17:55:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function johnLennonFacts(facts) {
   var a = 0
   var ping = []
   var d = "!!!"
-  while (a === facts.length) {
+  while (a < facts.length) {
     ping.push(facts[a] + d)
     a++
   }
